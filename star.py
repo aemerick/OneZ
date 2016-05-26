dde95a1730d5c2c8681155a4e2f38e09cdbf3186
aemerick <aemerick11@gmail.com>
2016-05-26 16:49:05 -0400

    fully tied into SE and Radiation data tables including fallback to black body curves for radiation properties. Tied into Starburst 99 stellar wind model.

--- star.py
+++ star.py
@@ -1,8 +1,15 @@
+
+__author__ = "aemerick <emerick@astro.columbia.edu>"
+
 # --- external ---
 import numpy as np
 
 # --- internal ---
 import data_tables as DT
+import radiation   as rad
+import physics     as phys
+
+from constants import CONST as const
 
 # ------- load the global tables -----------
 SE_TABLE  = DT.StellarEvolutionData()
@@ -19,8 +26,6 @@ class StarParticle:
         self.tform = tform
         self.id    = id
 
-
-        self._assign_properties()
 
         self.properties = {} # constant properties
         self.properties['status'] = 'live'
@@ -45,6 +50,8 @@ class Star(StarParticle):
     def __init__(self, *args, **kwargs):
         StarParticle.__init__(self, *args, **kwargs)
 
+        self._assign_properties()
+
     def evolve(self, t, dt):
         """
         Evolve the star
@@ -53,7 +60,8 @@ class Star(StarParticle):
         #
         # check and update Mdot_ej from stellar winds
         #
-        self.Mdot_ej = 0.0
+        self.stellar_wind_parameters()
+        self.Mdot_ej = self.properties['Mdot_wind']
 
         #
         # check age
@@ -61,30 +69,64 @@ class Star(StarParticle):
         age = t - self.tform
 
         if (age + dt > self.properties['lifetime']):
-            self.particle_properties['status'] = ['dead']
+            self.properties['status'] = ['dead']
+
+            
             
             #
             # conditional statements to turn particle into WD
             # or remnant
             #
 
-    @property
-    def surface_gravity(self):
-        return const.G * self.properties['M']/self.properties['R']**2
+    def mechanical_luminosity(self):
+        return self.properties['Mdot_wind'] * const.Msun * self.properties['v_wind']**2
 
-    @property
+    def surface_gravity(self):
+        return const.G * self.M * const.Msun / self.properties['R']**2
+
     def surface_area(self):
         return 4.0 * np.pi * self.properties['R']**2
+
+    def ionizing_photons(self, photon_type):
+
+        if not 'q' in photon_type:
+            if photon_type == 'HI':
+                photon_type = 'q0'
+            elif photon_tupe == 'HeI':
+                photon_type = 'q1'
+
+        if self.properties.haskey(photon_type):
+            return self.properties[photon_type] * self.surface_area()
+        else:
+            return 0.0
+
+    def fuv_luminosity(self):
+        if self.properties.haskey('FUV_flux'):
+            return self.properties['FUV_flux'] * self.surface_area()
+        else:
+            return 0.0
+
+    def stellar_wind_parameters(self, mode = None):
+
+        Mdot  = phys.s99_wind_mdot(self.properties['luminosity'], self.M_o,
+                                  self.properties['Teff'], self.Z)
+        vwind = phys.s99_wind_velocity( self.properties['luminosity'], self.M_o,
+                                        self.properties['Teff'], self.Z)
+
+        self.properties['Mdot_wind'] = Mdot
+        self.properties['v_wind']    = vwind
+
+
 
     def _assign_properties(self):
 
         p_list = ['luminosity', 'radius',
-                  'lifetime'  , 'l_fuv',
-                  'l_q1', 'l_q2', 'e_q1', 'e_q2']
+                  'lifetime'  , 'age_agb', 'L_FUV',
+                  'Q1', 'Q2', 'E_Q1', 'E_Q2']
 
         # update SE table to handle arrays 
 
-        self.properties['luminosity'] =\
+        self.properties['luminosity'] = const.Lsun *\
                    SE_TABLE.interpolate([self.M, self.Z], 'L')
         self.properties['Teff'] =\
                    SE_TABLE.interpolate([self.M, self.Z], 'Teff')
@@ -93,15 +135,50 @@ class Star(StarParticle):
         self.properties['lifetime'] =\
                    SE_TABLE.interpolate([self.M, self.Z], 'lifetime')
         self.properties['age_agb'] =\
-                   SE_table.interpolate([self.M, self.Z], 'age_agb')
+                   SE_TABLE.interpolate([self.M, self.Z], 'age_agb')
                                                   
         
+        Q0  = RAD_TABLE.interpolate([self.properties['Teff'], self.surface_gravity(),
+                                    self.Z], 'q0')
+        Q1  = RAD_TABLE.interpolate([self.properties['Teff'], self.surface_gravity(),
+                                    self.Z], 'q1')
+        FUV = RAD_TABLE.interpolate([self.properties['Teff'], self.surface_gravity(),
+                                    self.Z], 'FUV_flux')
+        E0  = rad.average_energy(const.E_HI/ const.eV_erg, self.properties['Teff'])
+        E1  = rad.average_energy(const.E_HeI/const.eV_erg, self.properties['Teff'])
+
+
+        use_blackbody = False
+        for a in [Q0, Q1, FUV]:
+            if a == 'offgrid':
+                use_blackbody = True
+                break;
+
+        if use_blackbody:
+            FUV = rad.fuv_flux_blackbody(self.properties['Teff'])
+            Q0  = rad.compute_blackbody_q0(self.properties['Teff'])
+            Q1  = rad.compute_blackbody_q1(self.properties['Teff'])
+
+            if self.M_o < 20.0:
+                corr_ind = 0
+            else:
+                corr_ind = 1
+
+            Q0  *= const.black_body_q0[corr_ind]
+            Q1  *= const.black_body_q1[corr_ind]
+            FUV *= const.black_body_fuv[corr_ind]
+
     
+        self.properties['Q0']    = Q0 * self.surface_area()
+        self.properties['E0']    = E0
+        self.properties['Q1']    = Q1 * self.surface_area()
+        self.properties['E1']    = E1
+        self.properties['L_FUV'] = FUV * self.surface_area()
+
         self.Mdot_ej = 0.0
 
 
 
-    def _stellar_evolution_grid_position()
 
 #def WhiteDwarf(StarParticle):
 
