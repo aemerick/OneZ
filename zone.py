9ae188b581963827624aab6cf6683fef02bf9726
aemerick <aemerick11@gmail.com>
2016-05-26 16:50:41 -0400

    working for basic simulations with radiating particles and winds, no chemistry. Data output works in both modes. physics not completely right

--- zone.py
+++ zone.py
@@ -6,13 +6,18 @@
     Purpose:
 
 """
+
+__author__ = "aemerick <emerick@astro.columbia.edu>"
+
 # external
 import numpy as np
+from collections import OrderedDict
 import pickle 
 
 # internal
 import imf  as imf
 import star as star
+from constants import CONST as const
 
 class Zone:
 
@@ -25,12 +30,15 @@ class Zone:
         self.imf = imf.salpeter()
 
         # some private things
-        self._t_last_dump    = 0.0
-        self._t_last_summary = 0.0
+        self._t_last_dump        = 0.0
+        self._t_last_summary     = 0.0
         self._cycle_number       = 0
         self._cycle_last_dump    = 0
         self._cycle_last_summary = 0
         self._output_number      = 0
+        self._summary_output_number = 0
+        self._summary_filename = 'summary_output.txt'
+
 
         self.t  = 0.0
         self.dt = 1.0 # Myr
@@ -38,6 +46,7 @@ class Zone:
         self.parameters = {}
 
         self._set_default_parameters()
+        self._summary_data = {}
         self.Mdot_ej = 0.0
 
     def evolve(self, tend):
@@ -67,6 +76,9 @@ class Zone:
            self.M_gas += (self.Mdot_in + self.Mdot_ej -\
                              self.Mdot_out) * self.dt - M_sf
 
+           if self.M_gas < 0:
+               self.M_gas = 0.0
+
            self.t += self.dt
            self._cycle_number += 1      
  
@@ -81,6 +93,7 @@ class Zone:
 
         self.Mdot_ej = 0.0
         self.Mdot_ej = np.sum([x.Mdot_ej for x in self.stars])
+        self.Mdot_ej = self.Mdot_ej * 1.0E6 * const.yr_to_s # msun / myr
 
     def _make_new_stars(self):
         
@@ -107,8 +120,10 @@ class Zone:
         if (not hasattr(self, '_global_id_counter')):
             self._global_id_counter = 0
 
+        num = self._global_id_counter
+
         self._global_id_counter += 1
-        return self._global_id_counter
+        return num
 
     def _compute_inflow(self):
         self.Mdot_in  = self.parameters['inflow_factor'] * self.Mdot_out
@@ -122,26 +137,26 @@ class Zone:
     def _check_output(self):
 
         # check for full write out
-        if( (self.t - self._t_last_dump) > self.parameters['dt_dump'] and
+        if( (self.t - self._t_last_dump) >= self.parameters['dt_dump'] and
             self.parameters['dt_dump'] > 0 ):
             self._t_last_dump = self.t
             self._write_full_dump()
 
         if( self._cycle_number == 0 or\
-           ((self._cycle_number - self._cycle_last_dump) > self.parameters['cycle_dump'])\
+           ((self._cycle_number - self._cycle_last_dump) >= self.parameters['cycle_dump'])\
            and self.parameters['cycle_dump'] > 0 ): 
             self._cycle_last_dump = self._cycle_number
             self._write_full_dump()
 
         # now check for partial writes
         if(  self._cycle_number == 0 or\
-            (self._cycle_number - self._cycle_last_summary > self.parameters['cycle_summary'])\
+            (self._cycle_number - self._cycle_last_summary >= self.parameters['cycle_summary'])\
             and self.parameters['cycle_summary'] > 0):
 
             self._cycle_last_summary = self._cycle_number
             self._write_summary_output()
 
-        if( (self.t - self._t_last_summary) > self.parameters['dt_summary'] and
+        if( ((self.t - self._t_last_summary) > self.parameters['dt_summary']) and
             self.parameters['dt_summary'] > 0 ):
             self._t_last_summary = self.t
             self._write_summary_output()
@@ -159,6 +174,33 @@ class Zone:
 
         self._output_number += 1
 
+
+    def _accumulate_summary_data(self):
+
+        self._summary_data = OrderedDict()
+
+        star_parse = lambda name: [x.properties[name] for x in self.stars]
+        sum_parse  = lambda name: np.sum(star_parse(name))
+
+        self._summary_data['t']       = self.t
+
+        self._summary_data['M_gas']   = self.M_gas
+        self._summary_data['M_DM']    = self.M_DM
+
+        self._summary_data['Z_gas']   = self.Z
+        self._summary_data['Z_star']  = np.average( [x.Z for x in self.stars])
+
+        M                             = [x.M for x in self.stars]
+        self._summary_data['M_star']  = np.sum(M)
+        self._summary_data['N_star']  = np.size(M)
+        self._summary_data['Mdot_ej'] = np.sum([x.Mdot_ej for x in self.stars])
+
+
+        self._summary_data['L_FUV'] = sum_parse('L_FUV')
+        self._summary_data['Q0']    = sum_parse('Q0')
+        self._summary_data['Q1']    = sum_parse('Q1')
+
+
     def _write_summary_output(self):
 
         # probably just do this as a line in a txt file
@@ -167,7 +209,27 @@ class Zone:
         # where the luminosities are summed over all star particles from stellar wind
         # supernova and radiation contributions
 
-        print "summary output"
+        self._accumulate_summary_data()
+
+        ncol = np.size(self._summary_data.keys())
+
+        
+
+        if self._summary_output_number == 0:
+            header = " " + " ".join(self._summary_data.keys()) + "\n"
+
+            f = open(self._summary_filename,'w')
+            f.write(header)
+        else:
+            f = open(self._summary_filename, 'a')
+
+
+        fmt = "%5.5E "*ncol + "\n"
+        f.write(fmt%(  tuple(self._summary_data.values()) ))
+
+        self._summary_output_number += 1
+        self._summary_data.clear()
+        f.close()
 
 
  
