c83ebf3ef02de0eab6d719ac56458134c5b41411
Andrew Emerick <aemerick11@gmail.com>
2018-04-30 17:14:52 -0400

    plot style updates

--- plots/radiation_properties.py
+++ plots/radiation_properties.py
@@ -1,13 +1,16 @@
+
+from galaxy_analysis.plot.plot_styles import *
+
 import numpy as np
-from matplotlib import rc
+#from matplotlib import rc
 
-fsize = 17
-rc('text', usetex=False)
-rc('font', size=fsize)#, ftype=42)
-line_width = 3
-point_size = 30
+#fsize = 17
+#rc('text', usetex=False)
+#rc('font', size=fsize)#, ftype=42)
+#line_width = 3
+#point_size = 30
 
-
+rc('font', size = 24)
 
 import matplotlib.pyplot as plt
 
@@ -29,6 +32,7 @@ def plot_property(name, IMF_weighted = False, xlim = None, ylim = None):
     label_dict = ptools._generate_label_dictionary()
 
     fig, ax = plt.subplots(1)
+    fig.set_size_inches(8,8)
 
     ls = ['-','--','-.',':','-']
 
@@ -44,7 +48,7 @@ def plot_property(name, IMF_weighted = False, xlim = None, ylim = None):
             ptemp = prop[i][prop[i] > 0.0]
             pmin = np.min(prop[prop>0.0])
             pmax = np.max(prop[prop>0.0])
-            ax.plot(mtemp, ptemp, ls = ls[i], lw = 3, color = 'black', label='Z= %.1E'%(z[i]))
+            ax.plot(mtemp, ptemp, ls = ls[i], lw = 2.0, color = 'black', label='Z= %.1E'%(z[i]))
     else:
     
 
@@ -53,7 +57,7 @@ def plot_property(name, IMF_weighted = False, xlim = None, ylim = None):
             if IMF_weighted:
                 y = y * config.zone.imf.imf(m) / np.sum(config.zone.imf.imf(m)) 
 
-            ax.plot(m, y, ls = ls[i], lw = 3, color = 'black', label='Z= %.1E'%(z[i]))
+            ax.plot(m, y, ls = ls[i], lw = 2.0, color = 'black', label='Z= %.1E'%(z[i]))
 
     ax.set_xlabel(r'Stellar Mass (M$_{\odot}$)')
 
@@ -83,16 +87,15 @@ def plot_property(name, IMF_weighted = False, xlim = None, ylim = None):
 
     ax.minorticks_on()
     ax.legend(loc='best')
-    fig.set_size_inches(8,8)
     plt.tight_layout()
     fig.savefig(name + '.png')
     plt.close()
 
 plot_property('agb_phase_length')
-plot_property('L_FUV', xlim = [1,100], ylim = [1.0E32, 1.0E39])
-plot_property('L_LW', xlim = [1,100], ylim = [1.0E32, 1.0E39])
-plot_property('Q0', xlim = [1,100], ylim = [1.0E40, 1.0E50])
-plot_property('Q1', xlim = [1,100], ylim = [1.0E40, 1.0E50])
+plot_property('L_FUV', xlim = [1,100], ylim = [1.0E35, 1.0E39])
+plot_property('L_LW', xlim = [1,100], ylim = [1.0E35, 1.0E39])
+plot_property('Q0', xlim = [1,100], ylim = [1.0E45, 1.0E50])
+plot_property('Q1', xlim = [1,100], ylim = [1.0E45, 1.0E50])
 plot_property('E0', xlim = [1,100], ylim = [14,34])
 plot_property('E1', xlim = [1,100], ylim = [14,34])
 plot_property('luminosity')
