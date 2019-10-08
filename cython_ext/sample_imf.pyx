c182fc26e75936daf679a856befd25d18d1d4977
Andrew Emerick <aemerick11@gmail.com>
2019-10-08 15:21:53 -0700

    first commit of cython extensions

---
+++ cython_ext/sample_imf.pyx
@@ -0,0 +1,50 @@
+#
+# code a search function here too
+#
+
+import numpy as np
+
+MAX_STARS = 100000
+
+cdef int find_bin(double[:] array, double x, int l, int r, int total):
+
+    cdef int w   = r-1
+    cdef int mid = l+w/2
+    print(w,mid,l,r,total,x,array[0],array[total-1])
+
+    if(w<=1):
+        if(mid<total-1):
+            if (x>=array[mid] and x <array[mid+1]):
+                return mid
+        if(mid>0):
+            if(x>=array[mid-1] and x<array[mid]):
+                return mid-1    
+    if (array[mid] > x):
+        return find_bin(array,x,l,mid-1,total)
+    elif (array[mid]<x):
+        return find_bin(array,x,mid+1,r,total)
+    else:
+        return mid
+
+    return -1
+
+def sample_imf(double[:] table, double M, 
+               int N,
+               double IMF_dm,
+               double IMF_start, double IMF_end,
+               int table_points):
+
+    cdef double[:] stars = np.zeros(int(M/IMF_start))
+    cdef int i = -1
+    cdef double total_mass = 0.0
+    cdef double rnum = 0.0
+    cdef int bin = 0
+
+    while total_mass < M:
+        i = i + 1
+        rnum = np.random.rand()
+        bin  = find_bin(table, rnum, 0, table_points, table_points)
+        stars[i]      = 10.0**(bin * IMF_dm + IMF_start)
+        total_mass    += stars[i]
+
+    return stars
