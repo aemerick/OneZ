750ba1dd203b8ae5aa3e1bf06564f603a52e11d2
Andrew Emerick <aemerick11@gmail.com>
2019-10-08 21:21:04 -0700

    better setup.py handling of cython and extensions. Need to clean this up a lot. Getting the hang of things

--- setup.py
+++ setup.py
@@ -1,17 +1,51 @@
 from setuptools import setup
-from setuptools.extension import Extension
+#from setuptools.extension import Extension
+
+import numpy as np
+
+import os
+import sys
+import subprocess
+
+#from distutils.core import setup
+from distutils.extension import Extension
 from Cython.Build import cythonize
+from Cython.Distutils import build_ext
+
+
+args = sys.argv[1:]
+
+# Make a `cleanall` rule to get rid of intermediate and library files
+if "cleanall" in args:
+    print("Deleting cython files...")
+    # Just in case the build directory was created by accident,
+    # note that shell=True should be OK here because the command is constant.
+    for p in ['./','./onezone/cython_ext/']:
+        subprocess.Popen("rm -rf " + p + "build", shell=True, executable="/bin/bash")
+        subprocess.Popen("rm -rf " + p + "*.c", shell=True, executable="/bin/bash")
+        subprocess.Popen("rm -rf " + p + "*.so", shell=True, executable="/bin/bash")
+
+    # Now do a normal clean
+    sys.argv[1] = "clean"
+
+# We want to always use build_ext --inplace
+if args.count("build_ext") > 0 and args.count("--inplace") == 0:
+    sys.argv.insert(sys.argv.index("build_ext")+1, "--inplace")
 
 cython_extensions = [
     Extension(
-        "cython_ext.sample_imf",
-        ["cython_ext/sample_imf.pyx"],
-        ),
+          "onezone.cython_ext.sample_imf",
+          ["onezone/cython_ext/sample_imf.pyx"],
+        )
+#    Extension(
+#           "onezone.cython_ext.cython_star",
+#          ["onezone/cython_ext/cython_star.pyx"],
+#        )
 ]
 
 setup(
       name="onezone",
-      version="",
+      version="0.1",
       author="Andrew Emerick",
       author_email="aemerick11@gmail.com",
       url="https://github.com/aemerick/onezone",
@@ -26,6 +60,6 @@ setup(
                 'cython',
                 'matplotlib',
                 'h5py'], # and others ... need to finish
-      ext_modules = cythonize("onezone/cython_ext/sample_imf.pyx"),
+      ext_modules = cythonize(cython_extensions) # cythonize("onezone/cython_ext/sample_imf.pyx"),
 #cython_extensions
 )
