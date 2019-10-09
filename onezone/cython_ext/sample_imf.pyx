317944aa385687ffc4d32473ef257df4b3c72b4e
Andrew Emerick <aemerick11@gmail.com>
2019-10-08 17:24:03 -0700

    bugfixes

--- onezone/cython_ext/sample_imf.pyx
+++ onezone/cython_ext/sample_imf.pyx
@@ -8,16 +8,18 @@ MAX_STARS = 100000
 
 cdef int find_bin(double[:] array, double x, int l, int r, int total):
 
-    cdef int w   = r-1
-    cdef int mid = l+w/2
-    print(w,mid,l,r,total,x,array[0],array[total-1])
+    cdef int w   = r - l
+    cdef int mid = l + w/2
+
+    if(x<array[0]):
+        return 0
 
     if(w<=1):
-        if(mid<total-1):
-            if (x>=array[mid] and x <array[mid+1]):
+        if(mid < total-1):
+            if ((x>=array[mid]) and (x<array[mid+1])):
                 return mid
         if(mid>0):
-            if(x>=array[mid-1] and x<array[mid]):
+            if((x>=array[mid-1]) and (x<array[mid])):
                 return mid-1    
     if (array[mid] > x):
         return find_bin(array,x,l,mid-1,total)
